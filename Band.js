69440161433dda8428b353ef35013e6d5654bac9
LoganHowat <lhowat624@hotmail.com>
2023-02-15 14:56:47 +0000

    Finishing the bonus tasks

--- Band.js
+++ Band.js
@@ -1,9 +1,11 @@
+const { DataTypes } = require('sequelize');
 const {Sequelize, sequelize} = require('./db');
 
 // TODO - define the Band model
 const Band = sequelize.define('band', {
     name: Sequelize.STRING,
-    genre: Sequelize.STRING
+    genre: Sequelize.STRING,
+    showCount: DataTypes.NUMBER
 });
 
 module.exports = {
--- index.test.js
+++ index.test.js
@@ -16,7 +16,8 @@ describe('Band and Musician Models', () => {
         // TODO - test creating a band
         let band1 = Band.create({
             name:"Beatles",
-            genre:"Rock"
+            genre:"Rock",
+            showCount:100
         })
         let foundBand = await Band.findAll({
             where:{
@@ -24,6 +25,8 @@ describe('Band and Musician Models', () => {
             }
         })
         expect(foundBand[0]['name']).toBe('Beatles');
+        expect(foundBand[0]['genre']).toBe('Rock');
+        expect(foundBand[0]['showCount']).toBe(100)
     })
 
     test('can create a Musician', async () => {
@@ -38,5 +41,28 @@ describe('Band and Musician Models', () => {
             }
         })
         expect(foundMusician[0]['name']).toBe('John Williams');
+    })
+    test('can destroy a Musician Instance', async () => {
+        let musician2 = Musician.create({
+            name:"Test",
+            instrument:"Orchestra"
+        });
+        (await musician2).destroy();
+        let foundTest = await Musician.findAll({
+            where:{
+                name: "Test"
+            }
+        })
+        expect(foundTest.length).toBe(0);
+    })
+
+    test('can update a Musician Instance', async () => {
+        let musician3 = Musician.create({
+            name:"Updating",
+            instrument:"Orchestra"
+        });
+        let updated = (await musician3).update({name:'Hans Zimmer'});
+        console.log((await musician3).dataValues.name)
+        expect((await musician3).dataValues.name).toBe('Hans Zimmer');//need to put the await musician3 in brackets together
     })
 })
